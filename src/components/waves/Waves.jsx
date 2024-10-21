46966c853af069553a239eb6b6f6ea968f983060
Jorge Espinosa <jyer94@gmail.com>
2024-10-21 14:12:15 -0600

    feat: commit inicial, lista de juegos

---
+++ src/components/waves/Waves.jsx
@@ -0,0 +1,10 @@
+import './Waves.css';
+
+export const Waves = () => {
+    return (
+        <div className="wave-container">
+            <div className="wave2 w2"></div>
+            <div className="wave w1"></div>
+        </div>
+    )
+}
