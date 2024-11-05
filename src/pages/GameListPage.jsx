8d8dc13b481d6d103d5679eb4d5402bbaf97ae5e
Jorge Espinosa <jyer94@gmail.com>
2024-11-04 23:08:21 -0600

    test: carga de imagenes con rutas relativas

--- src/pages/GameListPage.jsx
+++ src/pages/GameListPage.jsx
@@ -43,7 +43,6 @@ export const GameListPage = () => {
         }
 
         setlstFilteredItems(lstFiltered);
-        console.log('import.meta.env.VITE_PUBLIC_URL', import.meta.env.VITE_PUBLIC_URL);
     }
 
     useEffect(() => {
@@ -100,7 +99,7 @@ export const GameListPage = () => {
 
                     {lstFilteredItems.map(item =>
                         <div key={item.name} className='game'>
-                            <img src={`${import.meta.env.VITE_PUBLIC_URL}/img/games/${item.img}`}
+                            <img src={`./img/games/${item.img}`}
                                 alt={item.name}
                                 className='image'
                                 onError={(e) => {
