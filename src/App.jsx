1b10aea6be6da0caaf411d9ffaba8a91e811dc07
Jorge Espinosa <jyer94@gmail.com>
2024-11-10 02:23:45 -0600

    test: hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { GameListPage } from './pages/GameListPage'
 import { DicePage } from './pages/DicePage'
 import { PhasmophobiaFilterPage } from './pages/PhasmophobiaFilterPage'
@@ -9,13 +9,13 @@ import { PhasmophobiaGamePage } from './pages/PhasmophobiaGamePage'
 export const App = () => {
 
     return (
-        <BrowserRouter basename={import.meta.env.VITE_PUBLIC_URL}>
+        <HashRouter basename={import.meta.env.VITE_PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<GameListPage />} />
                 <Route path="/dice" element={<DicePage />} />
                 <Route path="/phasmophobia-filter" element={<PhasmophobiaFilterPage />} />
                 <Route path="/phasmophobia-game" element={<PhasmophobiaGamePage />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
