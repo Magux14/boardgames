a9a589535ca5c54cc93a61f412b2f7d6b5c3fa20
Jorge Espinosa <jyer94@gmail.com>
2024-11-15 14:21:38 -0600

    feat: ghost info

--- src/pages/PhasmophobiaFilterPage.jsx
+++ src/pages/PhasmophobiaFilterPage.jsx
@@ -21,6 +21,8 @@ export const PhasmophobiaFilterPage = () => {
 
     const [ghostListFiltered, setGhostListFiltered] = useState(phasmophobiaGhostList);
     const [cordura, setCordura] = useState(10);
+    const [folderSelected, setFolderSelected] = useState('tests');
+    const [selectedGhost, setSelectedGhost] = useState(null);
     const navigation = useNavigate();
 
     const handleCheckbox = (equipmentIndex) => {
@@ -53,9 +55,9 @@ export const PhasmophobiaFilterPage = () => {
             ghostListFiltered[i].selected = false
             if (i == selectedIndex) {
                 ghostListFiltered[i].selected = true;
+                setSelectedGhost(ghostListFiltered[i])
             }
         }
-
         setGhostListFiltered([...ghostListFiltered]);
     }
 
@@ -71,69 +73,117 @@ export const PhasmophobiaFilterPage = () => {
     return (
         <>
             <Header />
-            <div id="notebook-container">
-                <div id="test-container">
-                    <div id="cordura-container">
-                        <label className="cordura-title">Cordura</label>
-                        <div className='controllers'>
-                            <button onClick={(() => cordura > 0 ? setCordura(cordura - 1) : null)}>-</button>
-                            <label className="cordura-amount">{cordura > 0 ? cordura : 'MUERTO'}</label>
-                            <button onClick={(() => cordura < 99 ? setCordura(cordura + 1) : null)}>+</button>
-                        </div>
+            <div id="folder-container">
+                <div className="folder-options-container">
+                    <div className="empty">
 
                     </div>
-
-                    <div id="test-selection-container">
-                        <label>Pruebas</label>
-                        {
-                            filters.map(((equipment, index) =>
-                                <div key={equipment.name} className={`test-item ${equipment.status == 'discarded' ? 'discarded' : ''}`} onClick={() => handleCheckbox(index)}>
-                                    {
-                                        equipment.status == 'unselected' || equipment.status == 'discarded' ?
-                                            <CheckBoxOutlineBlankIcon />
-                                            : <CheckBoxIcon />
-                                    }
-                                    <label className='equipment-name'>{equipment.name}</label>
-                                </div>
-                            ))
-                        }
+                    <div className={`folder-option ${folderSelected == 'ghosts' ? 'folder-selected' : ''}`} onClick={(() => setFolderSelected('ghosts'))}>
+                        Fantasmas
                     </div>
-
-                    <div id="filtered-container">
-
-                        <label>Con las pruebas halladas, creemos que el fantasma es:</label>
-                        {
-                            ghostListFiltered.map(((ghost, index) =>
-                                <div
-                                    key={ghost.name}
-                                    className={`ghost-item ${!ghost.match ? 'no-match' : ''}`}
-                                    onClick={(() => handleSelected(index))}
-                                >
-                                    <label className={`${ghost.selected ? 'selected' : ''}`}>{ghost.name}</label>
-                                </div>
-                            ))
-                        }
+                    <div className={`folder-option ${folderSelected == 'tests' ? 'folder-selected' : ''}`} onClick={(() => setFolderSelected('tests'))}>
+                        Pruebas
                     </div>
                 </div>
+                <div id="notebook-container">
+                    {
+                        folderSelected == 'tests' &&
+                        <div id="test-container">
+                            <div id="cordura-container">
+                                <label className="cordura-title">Cordura</label>
+                                <div className='controllers'>
+                                    <button onClick={(() => cordura > 0 ? setCordura(cordura - 1) : null)}>-</button>
+                                    <label className="cordura-amount">{cordura > 0 ? cordura : 'MUERTO'}</label>
+                                    <button onClick={(() => cordura < 99 ? setCordura(cordura + 1) : null)}>+</button>
+                                </div>
 
+                            </div>
 
-                {
-                    !filters.find(item => item.status != 'selected') &&
-                    <div id="new-game-container">
-                        <div id="button-footer-container" onClick={(() => handleGoToNewGame())}>
-                            <button>New Game</button>
+                            <div id="test-selection-container">
+                                <label>Pruebas</label>
+                                {
+                                    filters.map(((equipment, index) =>
+                                        <div key={equipment.name} className={`test-item ${equipment.status == 'discarded' ? 'discarded' : ''}`} onClick={() => handleCheckbox(index)}>
+                                            {
+                                                equipment.status == 'unselected' || equipment.status == 'discarded' ?
+                                                    <CheckBoxOutlineBlankIcon />
+                                                    : <CheckBoxIcon />
+                                            }
+                                            <label className='equipment-name'>{equipment.name}</label>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+
+                            <div className="filtered-container">
+
+                                <label>Con las pruebas halladas, creemos que el fantasma es:</label>
+                                {
+                                    ghostListFiltered.map(((ghost, index) =>
+                                        <div
+                                            key={ghost.name}
+                                            className={`ghost-item ${!ghost.match ? 'no-match' : ''}`}
+                                            onClick={(() => handleSelected(index))}
+                                        >
+                                            <label className={`${ghost.selected ? 'selected' : ''}`}>{ghost.name}</label>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                            <br />
+
+                            {
+                                !filters.find(item => item.status != 'selected') &&
+                                <div id="new-game-container">
+                                    <div id="button-footer-container" onClick={(() => handleGoToNewGame())}>
+                                        <button>New Game</button>
+                                    </div>
+                                </div>
+                            }
+                        </div>
+
+                    }
+
+                    <div className="manual-ghost-container">
+                        {
+                            folderSelected == 'ghosts' &&
+                            <div className="filtered-container">
+                                {
+                                    ghostListFiltered.map(((ghost, index) =>
+                                        <div
+                                            key={ghost.name}
+                                            className={`ghost-item ${!ghost.match ? 'no-match' : ''}`}
+                                            onClick={(() => handleSelected(index))}
+                                        >
+                                            <label className={`${ghost.selected ? 'selected' : ''}`}>{ghost.name}</label>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        }
+
+                        <div className='equipment-ghost-info-container'>
+                            {
+                                phasmophobiaEquipment.map((equipment) => {
+                                    if (selectedGhost && selectedGhost[equipment.property] == true) {
+                                        return <div>
+                                            {equipment.name}
+                                        </div>
+                                    }
+                                })
+                            }
                         </div>
                     </div>
-                }
 
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
 
+                </div >
             </div >
         </>
     )
