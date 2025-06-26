fe17f8ebf6a41b8f914e36d2a780e4211239af4b
Jorge Espinosa <jorge.espinosa@definityfirst.com>
2025-06-25 22:21:18 -0600

    feat: save state resident evil

--- src/pages/resident-evil/ResidentEvilPage.jsx
+++ src/pages/resident-evil/ResidentEvilPage.jsx
@@ -1,12 +1,57 @@
+import { useEffect, useState } from 'react';
 import { BulletCounter } from './components/bullet-counter/BulletCounter';
+import { useSaveState } from '../../hooks/useSaveState';
 import './resident-evil.scss';
+
+const defaultGameState = {
+    life: 3,
+    gunBullets: 7,
+    shotgunBullets: 0,
+    machinegunBullets: 0,
+}
+
 export const ResidentEvilPage = () => {
+
+    const [gameState, setGameState] = useState(JSON.parse(JSON.stringify(defaultGameState)));
+    const { saveState, getLoadState } = useSaveState('resident-evil');
+
+    const setGunBullets = (value) => {
+        gameState.gunBullets = value;
+        setGameState({ ...gameState });
+        saveState(gameState);
+    }
+
+    const setShotgunBullets = (value) => {
+        gameState.shotgunBullets = value;
+        setGameState({ ...gameState });
+        saveState(gameState);
+    }
+
+
+    const setMachinegunBullets = (value) => {
+        gameState.machinegunBullets = value;
+        setGameState({ ...gameState });
+        saveState(gameState);
+    }
+
+    useEffect(() => {
+        const lastSaveState = getLoadState();
+        if (lastSaveState) {
+            setGameState(lastSaveState);
+        }
+    }, []);
+
     return (
         <div className="resident-evil__container">
             <div className="resident-evil__guns-container">
-                <BulletCounter name="pistola" bullets={0} />
-                <BulletCounter name="escopeta" bullets={0} />
-                <BulletCounter name="ametralladora" bullets={0} />
+                <div className="resident-evil__life-points-container">
+                    <div className="resident-evil__life-status">
+
+                    </div>
+                </div>
+                <BulletCounter name="pistola" bullets={gameState.gunBullets} setBullets={setGunBullets} />
+                <BulletCounter name="escopeta" bullets={gameState.shotgunBullets} setBullets={setShotgunBullets} />
+                <BulletCounter name="ametralladora" bullets={gameState.machinegunBullets} setBullets={setMachinegunBullets} />
             </div>
         </div>
     )
--- src/pages/resident-evil/components/bullet-counter/BulletCounter.jsx
+++ src/pages/resident-evil/components/bullet-counter/BulletCounter.jsx
@@ -1,21 +1,20 @@
 import React, { useState } from 'react';
 import './bullet-counter.scss';
 
-export const BulletCounter = ({ name, bullets, callbackSaveState }) => {
+export const BulletCounter = ({ name, bullets, setBullets }) => {
 
-    const [counter, setCounter] = useState(bullets);
     return (
         <div className="bullet-counter__container">
             <div className="bullet-counter__gun-name">
                 {name}
             </div>
             <div className="bullet-counter__controls">
-                <button onClick={(() => counter > 0 ? setCounter(counter - 1) : null)}>-</button>
+                <button onClick={(() => bullets > 0 ? setBullets(bullets - 1) : null)}>-</button>
                 <div className="bullet-counter__image-container">
                     <img src={`./img/resident-evil/balas-${name}.png`} />
-                    <div className="bullet-counter__counter-value">{counter}</div>
+                    <div className="bullet-counter__counter-value">{bullets}</div>
                 </div>
-                <button onClick={(() => counter < 99 ? setCounter(counter + 1) : null)}>+</button>
+                <button onClick={(() => bullets < 99 ? setBullets(bullets + 1) : null)}>+</button>
             </div>
         </div>
     )
